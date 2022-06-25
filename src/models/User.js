6770cbe37fb73384e6729fc9707b5fe3650d728c
surajverma2587 <surajverma2587@gmail.com>
2022-06-25 12:57:28 +0100

    add user model

---
+++ src/models/User.js
@@ -0,0 +1,80 @@
+const { Model, DataTypes } = require("sequelize");
+
+const connection = require("../config/connection");
+
+class User extends Model {}
+
+const schema = {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      len: [2, 50],
+      isAlpha: true,
+    },
+  },
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      len: [2, 50],
+      isAlpha: true,
+    },
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      len: [2, 20],
+      isAlphanumeric: true,
+    },
+  },
+  emailAddress: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
+    },
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      len: [8],
+      // is: ""
+    },
+  },
+  dateOfBirth: {
+    type: DataTypes.DATE,
+    allowNull: true,
+    validate: {
+      isDate: true,
+    },
+  },
+  profileImageUrl: {
+    type: DataTypes.STRING,
+    allowNull: true,
+    validate: {
+      isUrl: true,
+    },
+  },
+};
+
+const options = {
+  sequelize: connection,
+  timestamps: true,
+  freezeTableName: true,
+  modelName: "user",
+};
+
+User.init(schema, options);
+
+module.exports = User;
